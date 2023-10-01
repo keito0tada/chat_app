d71b17aea9e9f6e660f266c3990ce604a948f80c
keito0tada <keito0tada@gmail.com>
2023-10-01 19:58:30 +0900

    eslint, prettierの設定

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,24 +1,24 @@
-'use client'
+'use client';
 
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-import { useEffect, useState } from "react"
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useEffect, useState } from 'react';
 
-import type { Database } from "@/database.types"
+import type { Database } from '@/database.types';
 
-type Todo = Database['public']['Tables']['todo']['Row']
+type Todo = Database['public']['Tables']['todo']['Row'];
 
-export default function Page(){
-    const [todos, setTodos] = useState<Todo[] | null>(null);
-    const supabase = createClientComponentClient<Database>();
+export default function Page() {
+  const [todos, setTodos] = useState<Todo[] | null>(null);
+  const supabase = createClientComponentClient<Database>();
 
-    useEffect(() => {
-        const getData =async () => {
-            const {data, error} = await supabase.from('todo').select();
-            console.log(`unko ${error}`);
-            setTodos(data);
-        }
-        getData()
-    }, [])
-    console.log(todos);
-    return todos ? <pre>{JSON.stringify(todos)}</pre> : <p>loading</p>
+  useEffect(() => {
+    const getData = async () => {
+      const { data, error } = await supabase.from('todo').select();
+      console.log(`unko ${error}`);
+      setTodos(data);
+    };
+    getData();
+  }, []);
+  console.log(todos);
+  return todos ? <pre>{JSON.stringify(todos)}</pre> : <p>loading</p>;
 }
