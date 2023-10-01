99e58bf214a9ca8d3557da5dc19c68c21e706a91
keito0tada <keito0tada@gmail.com>
2023-10-02 00:10:42 +0900

    message表示、追加”

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 
 import { Database } from '@/database.types';
 
@@ -9,14 +9,22 @@ type Message = Database['public']['Tables']['messages']['Row'];
 
 export default function Page() {
     const [messages, setMessages] = useState<Message[] | null>(null);
+    const [authorId, setAuthorID] = useState<string | undefined>(undefined);
+    const [content, setContent] = useState<string>('');
     const supabase = createClientComponentClient<Database>();
     useEffect(() => {
         const getData = async () => {
             const { data, error } = await supabase.from('messages').select();
             setMessages(data);
+            const {
+                data: { user },
+            } = await supabase.auth.getUser();
+            setAuthorID(user?.id);
         };
         getData();
     }, []);
+    console.log(`user id ${authorId}`);
+    console.log(`content ${content}`);
     return (
         <>
             <p className="text-white">aaaaa</p>
@@ -27,6 +35,28 @@ export default function Page() {
                     </p>
                 );
             })}
+            <input
+                type="text"
+                id="content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                required
+            />
+            <button
+                className="bg-blue-500 text-white"
+                onClick={async (e) => {
+                    setContent('');
+                    if (authorId !== undefined) {
+                        const { data, error } = await supabase
+                            .from('messages')
+                            .insert({ content: content, author_id: authorId });
+                        console.log(data);
+                        console.log(error);
+                    }
+                }}
+            >
+                送信
+            </button>
         </>
     );
 }
