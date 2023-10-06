a1e4288a4bca7b1afd4177c3122edeae0e4fe134
keito0tada <keito0tada@gmail.com>
2023-10-06 13:47:39 +0900

    ルートページを修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,23 @@
-'use client';
-import Chat from '@/components/chat/Chat';
-import { Database } from '@/database.types';
-import {
-    User,
-    createClientComponentClient,
-} from '@supabase/auth-helpers-nextjs';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 export default function Page() {
-    const [user, setUser] = useState<User | null>(null);
-    const supabase = createClientComponentClient<Database>();
-    const router = useRouter();
-
-    useEffect(() => {
-        const getUserData = async () => {
-            const {
-                data: { user: userData },
-            } = await supabase.auth.getUser();
-            setUser(userData);
-            if (user !== null) {
-                router.push('/login');
-            }
-        };
-        getUserData();
-    }, []);
-
     return (
-        <div className="flex justify-start w-screen">
-            <div className="mx-0 bg-red-100">1</div>
-            <div className="mx-0 bg-blue-100">2</div>
+        <div className="flex flex-col justify-center py-5">
+            <label className="font-mono text-6xl">Chat App</label>
+            <div className="flex justify-between mt-3">
+                <Link
+                    className="w-32 h-8 pt-1 rounded-md text-center bg-blue-200 hover:bg-blue-100"
+                    href="/chat"
+                >
+                    ログイン
+                </Link>
+                <Link
+                    className="w-32 h-8 pt-1 rounded-md text-center bg-blue-200 hover:bg-blue-100"
+                    href="/signup"
+                >
+                    登録
+                </Link>
+            </div>
         </div>
     );
 }
